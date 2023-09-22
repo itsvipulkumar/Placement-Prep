db525877dc60a91c8b3d4f01032cfdb93dcf4044
Vipul Kumar <92398016+itsvipulkumar@users.noreply.github.com>
2023-09-22 22:47:43 +0530

    day 3

---
+++ LoveBabbar/RecursionDay10/Day3.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+int binarySearch(vector<int> &arr, int s, int e, int key)
+{
+    if (s > e)
+    {
+        return -1;
+    }
+    int mid = s + (e - s) / 2;
+    if (arr[mid] == key)
+    {
+        return mid;
+    }
+    else if (key > arr[mid])
+    {
+        return binarySearch(arr, mid + 1, e, key);
+    }
+    else
+        return binarySearch(arr, s, mid - 1, key);
+}
+int linearSearch(vector<int> &arr, int i, int key)
+{
+    if (i > arr.size() - 1)
+    {
+        return -1;
+    }
+
+    if (key == arr[i])
+        return i;
+    linearSearch(arr, i + 1, key);
+}
+int getSum(vector<int> &arr, int i, int sum)
+
+{
+
+    if (i == arr.size())
+    {
+        return sum;
+    }
+    sum += arr[i];
+    cout << arr[i] << " " << sum << endl;
+    getSum(arr, i + 1, sum);
+}
+bool isSorted(vector<int> &arr, int size)
+{
+
+    if (size == arr.size() - 1)
+    {
+        return true;
+    }
+    if (arr[size] > arr[size + 1])
+    {
+        return false;
+    }
+    else
+        return isSorted(arr, size + 1);
+}
+int main()
+{
+    vector<int> arr{1, 2, 3, 4, 5};
+    int n = arr.size();
+    // cout << isSorted(arr, size) << endl;
+
+    // cout << "Sum is : " << getSum(arr, 0,0) << endl;
+
+    //    cout<< linearSearch(arr,0,4)<<endl;
+    cout << binarySearch(arr, 0, n - 1, 4) << endl;
+}
